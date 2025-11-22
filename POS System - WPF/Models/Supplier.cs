47483cd5566aa6fb81f6ec41fdcfe421ba91cc62
Mario Medhat <87922718+Mario-Medhat@users.noreply.github.com>
2025-11-22 15:40:33 +0200

    updating SalesView

--- POS System - WPF/Models/Supplier.cs
+++ POS System - WPF/Models/Supplier.cs
@@ -12,9 +12,11 @@ namespace POS_System___WPF.Models
         public int SupplierID { get; set; }
 
         public string SupplierName { get; set; }
+        
         public string ContactFullName { get; set; }
 
         public string Phone { get; set; }
+        
         public string Email { get; set; }
 
         public List<Product> Products { get; set; }
