f69a0f0888e6b251c51e07b74c3ed86ac6c16b68
Mario Medhat <87922718+Mario-Medhat@users.noreply.github.com>
2025-11-27 17:17:59 +0200

    Changing the Target Framework
    Changing the Target Framework from Net 10.0 to be Net 9.0.

--- POS.Domain/Interfaces/IProductRepository.cs
+++ POS.Domain/Interfaces/IProductRepository.cs
@@ -4,6 +4,9 @@ namespace POS.Domain.Interfaces
 {
     public interface IProductRepository : IRepository<Product>
     {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task AddAsync(Product product);
         Task<List<Product>> GetLowStockProducts(int threshold);
     }
 }
